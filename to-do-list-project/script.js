1aca9b3287a518b7c7c640a87d7a31d5e410eaed
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-22 20:16:23 -0300

    to do list

---
+++ to-do-list-project/script.js
@@ -0,0 +1,29 @@
+// Add a li in an ol and reset input text value;
+const textInput = document.getElementById('texto-tarefa');
+const addBtn = document.getElementById('criar-tarefa');
+let orderedList = document.querySelector('.lista-tarefas');
+
+addBtn.addEventListener('click', function () {
+    let listItems = document.createElement('li');
+    listItems.innerText = textInput.value;
+    orderedList.appendChild(listItems);
+    
+    textInput.value = '';
+
+    listItems.addEventListener('click', function () {
+        let selected = document.querySelector('.selected');
+            listItems.classList.toggle('selected')
+    })
+
+    listItems.addEventListener('dblclick', function(event) {
+        let completed = document.querySelector('.completed');
+        listItems.classList.toggle('completed');
+    })
+});
+
+// Button delete all list's items
+let deleteAllBtn = document.getElementById('apaga-tudo');
+
+deleteAllBtn.addEventListener('click', function () {
+    orderedList.innerHTML = '';
+})
